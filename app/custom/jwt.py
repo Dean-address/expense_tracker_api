b2fea8892c9eb84efa639dd6e0fc21c10bddc397
Alabi Khalid <alabikhalid.12@gmail.com>
2025-01-21 04:54:22 +0100

    change project to implement a design pattern

--- app/custom/jwt.py
+++
@@ -1,9 +0,0 @@
-from datetime import timedelta
-
-SIMPLE_JWT = {
-    "AUTH_HEADER_TYPES": ("Bearer",),
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
-    "USER_ID_FIELD": ("user_id"),
-}
